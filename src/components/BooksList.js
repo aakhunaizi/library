7ec8bae31704aa937c2ec6c41b9bc7c11f984726
JannatJameel <j_annat@outlook.com>
2021-02-03 18:43:43 +0300

    books list

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -4,20 +4,19 @@ import Tag from "./Tag";
 const BooksList = () => {
   const books = useSelector((state) => state.books);
 
-  //   const booksList = books.map((book) => {
-  //     <div className="row">
-  //       <Tag book={book} key={book.id} />
-  //     </div>;
-  //   });
+    const booksList = books.map((book) => 
+        <Tag book={book} key={book.id} />
+    );
 
   return (
     <div class="container-fluid">
       {/* Page Heading */}
       <div className="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 className="h3 mb-0 text-gray-800">My Books List</h1>
+        <h1 className="h3 mb-0 text-gray-800">My Books</h1>
       </div>
-      <div>
-        <Tag />
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {booksList}
+        {/* <Tag name={"Hellooo"}/> */}
       </div>
     </div>
   );
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,22 +1,25 @@
-const Tag = () => {
+const Tag = ({book}) => {
+  let status = "";
+  (book.available)? status = "Available" : status = "Borrowed";
+
   return (
-    <div style={{ display: "flex" }}>
-      <div className="card" style={{ width: "18rem" }}>
+    <div className="col-sm-6 mb-4">
+      <div className="card h-100">
         <img
           className="card-img-top"
           src="https://i.pinimg.com/736x/18/0e/fc/180efc3227b70e92b88fd3e71826fbad.jpg"
           alt="Shrek"
         />
         <div className="card-body">
-          <h5 className="card-title">Shrek</h5>
-          <p className="card-text">The king of the swamps</p>
+          <h5 className="card-title" style={{fontWeight: "bold"}}>{book.title}</h5>
+          <p className="card-text">{book.genre.join(", ")}</p>
         </div>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item">Where's donkey?</li>
+          <li className="list-group-item">{status}</li>
         </ul>
         <div className="card-body">
           <a href="#" className="btn btn-primary">
-            Go to swamp
+            More Details
           </a>
         </div>
       </div>
