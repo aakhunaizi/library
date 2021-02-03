989180865377c8c3dc8d663b0bcb457dbc4dafb5
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-02-03 17:49:37 +0300

    Added card

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,23 +1,27 @@
 const Tag = () => {
-
-    return (
-        <div className="col-xl-3 col-md-6 mb-4">
-            <div className="card border-left-primary shadow h-100 py-2">
-                <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                        <div className="col mr-2">
-                            {/* <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Earnings (Monthly)</div> */}
-                            <div className="h5 mb-0 font-weight-bold text-primary text-uppercase mb-1">Hellooooo</div>
-                        </div>
-                        <div className="col-auto">
-                            <i className="fas fa-calendar fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div style={{ display: "flex" }}>
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://i.pinimg.com/736x/18/0e/fc/180efc3227b70e92b88fd3e71826fbad.jpg"
+          alt="Shrek"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Shrek</h5>
+          <p className="card-text">The king of the swamps</p>
         </div>
-    );
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Where's donkey?</li>
+        </ul>
+        <div className="card-body">
+          <a href="#" className="btn btn-primary">
+            Go to swamp
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Tag;
