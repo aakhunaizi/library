8e100937a4e79e2b85954945ad3624d9e1cbfcd9
JannatJameel <j_annat@outlook.com>
2021-02-04 08:19:40 +0300

    woooow

--- src/components/BookHistory.js
+++ src/components/BookHistory.js
@@ -6,12 +6,16 @@ const BookHistory = ({member}) => {
 
     return (
         <div className="card mb-3 py-10 border-bottom-dark">
+            <Link to={`/members/${member.slug}`} style={{textDecoration: "none"}}>
                 <div className="card-body">
                     {fullName}
                     <div style={{float: "right"}}>
+                        <Link to={`/members/${member.slug}`}>
                             <button className="fas btn btn-outline-primary" style={{fontSize:"12px"}}>View Profile</button>          
+                        </Link>
                     </div>
                 </div>
+            </Link>
         </div>
     );
 };
--- src/components/BorrowedBook.js
+++ src/components/BorrowedBook.js
@@ -1,14 +1,18 @@
-import { MdRemoveCircle } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const BorrowedBook = ({book}) => {
     return (
         <div className="card mb-3 py-10 border-bottom-dark">
-            <div className="card-body">
-                {book.title}
-                <div style={{float: "right"}}>
-                    <button className="fas btn btn-outline-danger" style={{fontSize:"12px"}}><MdRemoveCircle/>Return</button>          
+            <Link to={`/books/${book.slug}`} style={{textDecoration: "none"}}>
+                <div className="card-body">
+                    {book.title}
+                    <div style={{float: "right"}}>
+                        <Link to={`/books/${book.slug}`}>
+                            <button className="fas btn btn-outline-danger" style={{fontSize:"12px"}}>Return</button>          
+                        </Link>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 };
